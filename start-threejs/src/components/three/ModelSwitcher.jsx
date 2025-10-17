c7438b22a7d63876631af005b8d17b62840a4b59
chiu3101 <tandleamruta33@gmail.com>
2025-10-17 15:09:06 +0530

    Product View in Space Gray & Dark

--- start-threejs/src/components/three/ModelSwitcher.jsx
+++ start-threejs/src/components/three/ModelSwitcher.jsx
@@ -27,10 +27,13 @@ const moveGroup = (group , x) => {
 //a custom hook coming from gsap which makes it easier to work with gsap using jsx
 
 const ModelSwitcher = ({scale , isMobile}) => {
+    const SCALE_LARGE_DESKTOP = 0.08;
+    const SCALE_LARGE_MOBILE = 0.05;
+
     const smallMacbookRef = useRef();
     const largeMacbookRef = useRef();
 
-    const showLargeMacbook = scale === 0.08 || scale === 0.05;
+    const showLargeMacbook = scale === SCALE_LARGE_DESKTOP || scale === SCALE_LARGE_MOBILE;
 
     useGSAP(() => {
         if(showLargeMacbook){
